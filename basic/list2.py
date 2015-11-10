5fc9b0b8ed7db805bfe1b503d995b9e31b9c05d9
Grier Phillips <GrierPhillips@users.noreply.github.com>
2015-11-10 14:55:32 -0500

    list2.py
    Additional list manipulation exercises

---
+++ basic/list2.py
@@ -0,0 +1,61 @@
+# D. Given a list of numbers, return a list where
+# all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
+# modify the passed in list.
+def remove_adjacent(nums):
+	reduced = []
+	for num in nums:
+		if len(reduced) == 0 or num != reduced[-1]:
+			reduced.append(num)
+	return reduced
+
+
+# E. Given two lists sorted in increasing order, create and return a merged
+# list of all the elements in sorted order. You may modify the passed in lists.
+# Ideally, the solution should work in "linear" time, making a single
+# pass of both lists.
+def linear_merge(list1, list2):
+	combined = []
+	#compare lists to identify smallest numbers and append it to combined
+	while len(list1) and len(list2):
+		if list1[-1] > list2[-1]:
+			combined.append(list1.pop(-1))
+		else:
+			combined.append(list2.pop(-1))
+	#one list may have remaining presorted sequences, add these.
+	combined.extend(list1)
+	combined.extend(list2)
+	#reverse the list
+	combined = combined[::-1]
+	return combined
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+  print('remove_adjacent')
+  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+  test(remove_adjacent([]), [])
+
+  print
+  print('linear_merge')
+  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+       ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+
+if __name__ == '__main__':
+  main()
